3dfa58d2e8dc73a6d1af1115dc87c3149dfe9b0f
davidmugisho <davidmugisho243@gmail.com>
2024-08-31 09:06:19 +0000

    jgj

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
-const image = { uri: 'https://legacy.reactjs.org/logo-og.png' };
+const image = { uri: 'https://i.pinimg.com/1200x/00/46/15/00461568c80482f4d6ebbb0fe180eaeb.jpg' };
 
 export default function App() {
   return (
