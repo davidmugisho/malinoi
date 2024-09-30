d8370499d76cc3eb4fdf7321103d44d029dea282
davidmugisho <davidmugisho243@gmail.com>
2024-09-30 09:23:25 +0200

    image

--- App.js
+++ App.js
@@ -2,28 +2,19 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
-const image = { uri: 'https://i.pinimg.com/1200x/00/46/15/00461568c80482f4d6ebbb0fe180eaeb.jpg' };
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <Text style={styles.text}>Inside</Text>
+      <ImageBackground source={require("./assets/Beige Minimal Pet Dog Instagram Story.jpg")} 
+      blurRadius={3} style={{flex:1, justifyContent:'center'}} resizeMode='center' >
+        <Text style={styles.text}></Text>
       </ImageBackground>
-    </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection:1
-  },
+
   text: {
     color: 'white',
     fontSize: 42,
