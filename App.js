867a578931783ed3e74c2380e0352524cd6137d3
davidmugisho <davidmugisho243@gmail.com>
2024-09-30 19:54:39 +0200

    axios

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Splash from "./screens/Splash";
+import Signup from './screens/Signup';
+import Login from './screens/Login';
+import Main from './screens/Main'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
@@ -14,6 +17,17 @@ const App = () => {
       <Stack.Screen options={{
         headerShown: false
       }} name="Splash" component={Splash} />
+         <Stack.Screen name="Signup" component={Signup} options={{
+        headerShown: false
+      }} />
+
+      <Stack.Screen name="Login" component={Login} options={{
+        headerShown: false
+      }} />
+
+      <Stack.Screen name="Main" component={Main} options={{
+        headerShown: false
+      }} />
 
 </Stack.Navigator>
 </NavigationContainer>
